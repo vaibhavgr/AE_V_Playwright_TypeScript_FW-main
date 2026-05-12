d2cc1296b388412ced5e0829cd57f50380887ab5
VinodBhusal <vinodbhusal9@gmail.com>
2026-05-13 01:17:45 +0530

    Test Rejection2

--- tests/cartTests/cart.spec.ts
+++ tests/cartTests/cart.spec.ts
@@ -7,5 +7,5 @@ test.only('verifyProductPage', async ({ loginPage, registrationPage }) => {
     await loginPage.verifyUserLogin(existingUser.existingEmail, existingUser.existingPassword);
     console.log("login successful");
     console.log("Hey its from main branch");
-    console.log("Reject this PR");
+    console.log("Reject this PR2");
 })

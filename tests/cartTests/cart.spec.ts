3bb290d85bb03e08a208013ede7e77bafa6c2fc6
VinodBhusal <vinodbhusal9@gmail.com>
2026-05-13 01:04:12 +0530

    Test Rejection

--- tests/cartTests/cart.spec.ts
+++ tests/cartTests/cart.spec.ts
@@ -5,5 +5,7 @@ test.only('verifyProductPage', async ({ loginPage, registrationPage }) => {
     const existingUser = existingUserEmail();
     await registrationPage.goto();
     await loginPage.verifyUserLogin(existingUser.existingEmail, existingUser.existingPassword);
-    console.log("login successful")
+    console.log("login successful");
+    console.log("Hey its from main branch");
+    console.log("Reject this PR");
 })
